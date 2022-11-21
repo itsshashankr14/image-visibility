d2b43b8a576ea02a1f75451f0fd25a0bef283714
mohan1345 <118711895+mohan1345@users.noreply.github.com>
2022-11-21 11:59:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// function visible(){
+//     let dem = document.getElementById("alian").style;
+//     if (dem.visibility == "visible"){
+//         alian.style.visibility = "hidden";
+//     }else{
+//         alian.style.visibility = "visible";
+//     }
+// }
+
+
+
+
+
+
+function visible(){
+    alian.style.visibility = "visible";
+}
+function hide(){
+    alian.style.visibility = "hidden";
+}
